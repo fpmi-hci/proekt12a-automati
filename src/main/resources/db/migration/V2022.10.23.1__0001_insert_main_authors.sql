f5c20f79945aa4f27d3e1c2cd8b21ff3ca2999be
Andrei_Kuksa <v-akuksa@expedia.com>
2022-11-03 19:33:46 +0300

    Fix flyway migrations

--- src/main/resources/db/migration/V2022.10.23.1__0001_insert_main_authors.sql
+++ src/main/resources/db/migration/V2022.10.23.1__0001_insert_main_authors.sql
@@ -1,8 +1,6 @@
 INSERT INTO public.application_user (id, email, password, icon_path, name, role, is_blocked)
-VALUES(1,	'andrey20011005@gmail.com',	'df683ef594107dde605c30a70873a92a','',		'JOHN_CENA',	'ADMIN', 'false')
-ON CONFLICT DO NOTHING;
+VALUES(1,	'andrey20011005@gmail.com',	'df683ef594107dde605c30a70873a92a','',		'JOHN_CENA',	'ADMIN', 'false');
 
 INSERT INTO public.application_user (id, email, password, icon_path, name, role, is_blocked)
-VALUES(2,	'the_flame_of_punisher@mail.ru',	'7b654e86119da8d5b3e370fd1ce0acc6',	'','JOHN XINA',	'USER',	'false')
-ON CONFLICT DO NOTHING;
+VALUES(2,	'the_flame_of_punisher@mail.ru',	'7b654e86119da8d5b3e370fd1ce0acc6',	'','JOHN XINA',	'USER',	'false');
 
