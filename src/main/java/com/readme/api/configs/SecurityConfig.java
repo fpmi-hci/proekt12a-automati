25b1c19e9e1547052daa1627ac70105bb9506d14
Andrei_Kuksa <v-akuksa@expedia.com>
2022-11-13 20:27:00 +0300

    fix cors

--- src/main/java/com/readme/api/configs/SecurityConfig.java
+++ src/main/java/com/readme/api/configs/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
